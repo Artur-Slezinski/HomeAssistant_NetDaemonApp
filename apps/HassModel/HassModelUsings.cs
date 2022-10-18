04a1f3ec533f46caa36dcc28fe98da3d36bfdf3d
Artur <as055959@student.ath.edu.pl>
2022-10-18 22:04:09 +0200

    Clone to GitHub

---
+++ apps/HassModel/HassModelUsings.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Reactive.Linq;
+
+global using Microsoft.Extensions.Logging;
+
+global using NetDaemon.Extensions.Scheduler;
+
+global using NetDaemon.HassModel.Entities;
+global using NetDaemon.HassModel.Extensions;
+global using NetDaemon.HassModel.Entities.Core;
+global using NetDaemon.HassModel.Integration;
---
+++ apps/HassModel/TempAndHumidity/TempAndHumidity.cs
@@ -0,0 +1,56 @@
+﻿// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+
+
+using HomeAssistantGenerated;
+using NetDaemon.HassModel;
+using Serilog;
+
+
+namespace HassModel;
+[NetDaemonApp]
+
+public class TempAndHumidity
+{
+
+    private readonly ILogger<TempAndHumidity> _log;
+    private readonly Entities _myEntities;
+    private readonly Services _services;
+
+    public TempAndHumidity(IHaContext ha)
+    {
+        var _myEntities = new Entities(ha);
+        var _services = new Services(ha);
+
+        //ha.Entity("switch.nodemcu_internal_led")
+        //    .StateChanges().Where(e => e.New?.State == "off")
+        //    .Subscribe(_ => ha.CallService("notify", "persistent_notification", data: new { message = "LED", title = "ON" }));
+
+        upTemp(_myEntities, _services);
+        downTemp(_myEntities, _services);
+        // InformTemp(_services);
+    }
+
+    private void upTemp(Entities entities, Services services)
+    {
+        NumericSensorEntity outdoorTemperature = entities.Sensor.Outdoortemp;
+        outdoorTemperature
+            .StateChanges()
+            .Where(e => outdoorTemperature.AsNumeric().State <= 25.0)
+            .Subscribe(_ => services.Switch.TurnOn(ServiceTarget.FromEntities("switch.outdoormcuinternalled", "switch.outdoormcuinternalled")));
+    }
+
+    private void downTemp(Entities entities, Services services)
+    {
+        NumericSensorEntity outdoorTemperature = entities.Sensor.Outdoortemp;
+        outdoorTemperature
+            .StateChanges()
+            .Where(e => outdoorTemperature.AsNumeric().State > 25.0)
+            .Subscribe(_ => services.Switch.TurnOff(ServiceTarget.FromEntities("switch.outdoormcuinternalled", "switch.outdoormcuinternalled")));
+    }
+
+    public void InformTemp(Services services)
+    {
+        services.Switch.TurnOn(ServiceTarget.FromEntities("switch.outdoormcuinternalled", "switch.outdoormcuinternalled"));
+    }
+}
